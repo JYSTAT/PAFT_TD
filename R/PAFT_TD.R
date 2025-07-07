2baf98517a1643941e2b37eb0606f1fc4814e34e
JYSTAT <jystat@yu.ac.kr>
2025-07-08 07:50:14 +0900

    PAFT_TD.R

--- R/PAFT_TD.R
+++ R/PAFT_TD.R
@@ -28,8 +28,7 @@ PAFT_TD <- function(formula, data, id_var = "ID",
                     initi = FALSE, beta = NA, sigma_init = NA,
                     tol = 1.0e-5, maxiter = 2000,
                     algorithm = "NLOPT_LN_COBYLA") {
-  
- 
+
   # --- Distribution-specific log-likelihood component --- #
   get_loglik_function <- function(dist) {
     switch(dist,
@@ -41,22 +40,22 @@ PAFT_TD <- function(formula, data, id_var = "ID",
            stop("Unsupported distribution: ", dist))
   }
   loglik_fn <- get_loglik_function(dist)
-  
+
   # --- Parse survival formula --- #
   mf <- model.frame(formula, data)
   surv_obj <- model.response(mf)
   if (!inherits(surv_obj, "Surv") || attr(surv_obj, "type") != "counting") {
     stop("Formula must be of the form Surv(start, stop, event) ~ covariates")
   }
-  
+
   if (!(id_var %in% names(data))) {
     stop(paste0("The data must contain the ID variable '", id_var, "'"))
   }
-  
+
   # --- Check if time-independent --- #
-  id_count <- count(data, !!sym(id_var))
+  id_count <- dplyr::count(data, !!rlang::sym(id_var))
   is_time_indep <- all(id_count$n == 1)
-  
+
   if (is_time_indep) {
     data$start <- 0
     data$stop  <- surv_obj[, 2]
@@ -71,7 +70,7 @@ PAFT_TD <- function(formula, data, id_var = "ID",
                 method = "survreg",
                 message = paste("Time-independent data fitted using survreg() with dist =", dist)))
   }
-  
+
   # --- Prepare design matrix --- #
   start_time <- surv_obj[, 1]
   stop_time  <- surv_obj[, 2]
@@ -79,76 +78,76 @@ PAFT_TD <- function(formula, data, id_var = "ID",
   X_mat_full <- model.matrix(formula, data)
   X_mat <- X_mat_full[, -1, drop = FALSE]
   covariate_names <- colnames(X_mat)
-  
+
   data$start <- start_time
   data$stop  <- stop_time
   data$delta <- event
   for (j in seq_along(covariate_names)) {
     data[[covariate_names[j]]] <- X_mat[, j]
   }
-  
+
   # --- Initial values --- #
   if (initi) {
     init_beta <- beta
     init_sigma <- sigma_init
   } else {
-    data_last <- data %>% group_by(.data[[id_var]]) %>% arrange(stop) %>% slice(n()) %>% ungroup()
+    data_last <- data %>% dplyr::group_by(.data[[id_var]]) %>% dplyr::arrange(stop) %>% dplyr::slice(n()) %>% dplyr::ungroup()
     X_last <- as.matrix(data_last[, covariate_names])
     fit_init <- survreg(Surv(stop, delta) ~ X_last, data = data_last, dist = dist)
     init_beta <- fit_init$coefficients
     init_sigma <- fit_init$scale
   }
-  
+
   # --- Negative log-likelihood --- #
   neg_log_likelihood <- function(params) {
     intercept <- params[1]
     betas <- params[2:(length(covariate_names) + 1)]
     sigma <- params[length(params)]
-    
-    obs_time <- data %>% group_by(.data[[id_var]]) %>% slice(n()) %>% ungroup() %>%
-      select(!!sym(id_var), stop) %>% rename(obs_time = stop)
-    dat_ext <- data %>% left_join(obs_time, by = id_var) %>% arrange(.data[[id_var]], start)
-    
+
+    obs_time <- data %>% dplyr::group_by(.data[[id_var]]) %>% dplyr::slice(n()) %>% dplyr::ungroup() %>%
+      dplyr::select(!!rlang::sym(id_var), stop) %>% dplyr::rename(obs_time = stop)
+    dat_ext <- data %>% dplyr::left_join(obs_time, by = id_var) %>% dplyr::arrange(.data[[id_var]], start)
+
     Z_mat <- as.matrix(dat_ext[, covariate_names])
     lin_pred <- -Z_mat %*% betas
     dat_ext$psi_comp <- (dat_ext$stop - dat_ext$start) * exp(lin_pred)
-    
-    psi_df <- dat_ext %>% group_by(.data[[id_var]]) %>%
-      summarise(psi = sum(psi_comp), .groups = "drop") %>% arrange(.data[[id_var]])
-    
-    Z_last <- dat_ext %>% group_by(.data[[id_var]]) %>% slice(n()) %>%
-      ungroup() %>% select(all_of(covariate_names)) %>% as.matrix()
+
+    psi_df <- dat_ext %>% dplyr::group_by(.data[[id_var]]) %>%
+      dplyr::summarise(psi = sum(psi_comp), .groups = "drop") %>% dplyr::arrange(.data[[id_var]])
+
+    Z_last <- dat_ext %>% dplyr::group_by(.data[[id_var]]) %>% dplyr::slice(n()) %>%
+      dplyr::ungroup() %>% dplyr::select(all_of(covariate_names)) %>% as.matrix()
     psi_d <- exp(-Z_last %*% betas)
-    
-    delta_df <- dat_ext %>% group_by(.data[[id_var]]) %>% slice(n()) %>%
-      ungroup() %>% select(!!sym(id_var), delta)
-    
-    final_df <- psi_df %>% mutate(psi_d = as.vector(psi_d)) %>%
-      left_join(delta_df, by = id_var) %>% arrange(.data[[id_var]])
-    
+
+    delta_df <- dat_ext %>% dplyr::group_by(.data[[id_var]]) %>% dplyr::slice(n()) %>%
+      dplyr::ungroup() %>% dplyr::select(!!rlang::sym(id_var), delta)
+
+    final_df <- psi_df %>% dplyr::mutate(psi_d = as.vector(psi_d)) %>%
+      dplyr::left_join(delta_df, by = id_var) %>% dplyr::arrange(.data[[id_var]])
+
     log_psi <- log(final_df$psi)
     z <- (log_psi - intercept) / sigma
     L <- loglik_fn(z, final_df$psi, final_df$psi_d, final_df$delta, sigma)
     -sum(log(pmax(L, 1.0e-20)))
   }
-  
+
   # --- Optimize using nloptr --- #
   init_params <- c(init_beta, init_sigma)
   bound_size <- max(abs(init_params)) * 10
   lower_bounds <- rep(-bound_size, length(init_params))
   upper_bounds <- rep(bound_size, length(init_params))
-  
-  res <- nloptr(x0 = init_params,
+
+  res <- nloptr::nloptr(x0 = init_params,
                 eval_f = neg_log_likelihood,
                 lb = lower_bounds,
                 ub = upper_bounds,
                 opts = list(algorithm = algorithm,
                             xtol_rel = tol,
                             maxeval = maxiter))
-  
+
   est_params <- res$solution
   names(est_params) <- c("Intercept", covariate_names, "sigma")
-  
+
   return(list(EstBetas = est_params,
               method = "PAFT_TD",
               itr = res$iterations,
